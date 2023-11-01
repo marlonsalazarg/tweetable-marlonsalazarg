741a54d2b812c2137b33b6e7c1db50856e335fe1
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-01 13:35:40 -0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: :callbacks }
+  resources :likes
+  resources :tweets
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "tweets#index"
 end
